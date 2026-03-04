59c2c5a2acae5fb3aa842fd8d4008f45d474c5e4
ContactGroundControl <contact@groundcontrol-llc.com>
2026-03-04 16:42:43 -0600

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "system-ui, Arial" }}>
+      <h1>Ground Control LLC</h1>
+      <p>Site is deploying…</p>
+    </main>
+  );
+}
